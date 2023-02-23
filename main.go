1a43779b4ff88c8d6680abc6781f8b14443355ef
madan-dinata <madanmr48@gmail.com>
2023-02-23 12:39:18 +0700

    connect to postgresql

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	var err error
+	db, err := sql.Open("postgres", "user=postgres password=madan  dbname=share-moment sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		panic(err)
+	} else {
+		fmt.Println("DB Connected...")
+	}
+
 	e := echo.New()
+	
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World Echo Golang")
 	})
