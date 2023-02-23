1919fde8a0e54add8f4ae3e5a48d7d4bd30bf436
madan-dinata <madanmr48@gmail.com>
2023-02-23 09:24:58 +0700

    adding air for restart server and add middleware

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello World")
+		return c.String(http.StatusOK, "Hello World Echo Golang")
 	})
+	e.Use(middleware.Logger())
 	e.Logger.Fatal(e.Start(":5000"))
 }
